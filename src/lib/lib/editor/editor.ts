5d23b194586b8d3338b1c2d3faf53d35b67079ff
tanbo <464328895@qq.com>
2019-08-02 23:09:46 +0800

    feat:inline 标签包裹与反包裹基本实现

--- src/lib/lib/editor/editor.ts
+++ src/lib/lib/editor/editor.ts
@@ -59,95 +59,122 @@ export class Editor {
   }
 
   private wrapInlineElement(tag: string) {
-    const self = this;
-
-    function wrapper(range: Range, selection: Selection, doc: Document, tag: string) {
-      const startWrapper = self.findParentByTagName(range.startContainer as HTMLElement, tag);
-      const endWrapper = self.findParentByTagName(range.endContainer as HTMLElement, tag);
-      if (startWrapper) {
-        range.setStartAfter(startWrapper);
-      }
-      if (endWrapper) {
-        range.setEndBefore(endWrapper);
-      }
-      if (range.startContainer === range.endContainer && range.commonAncestorContainer.nodeType === 3) {
-        range.surroundContents(document.createElement(tag));
-      } else {
-        self.getTextNodes(range.commonAncestorContainer as HTMLElement, tag)
-          .filter(item => selection.containsNode(item))
-          .forEach(item => {
-            if (item.textContent === '') {
-              item.parentNode.removeChild(item);
-              return;
-            }
-            const temporaryRange = doc.createRange();
-            temporaryRange.selectNode(item);
-            if (item === range.startContainer) {
-              temporaryRange.setStart(item, range.startOffset);
-            } else if (item === range.endContainer) {
-              temporaryRange.setEnd(item, range.endOffset);
-            }
-            temporaryRange.surroundContents(document.createElement(tag));
-            temporaryRange.detach();
-          });
-      }
-    }
-
-    function reverse(container: HTMLElement, range: Range, doc: Document, tag: string) {
-      Array.from(container.childNodes).forEach(node => {
-        if (!selection.containsNode(node) && node !== range.startContainer && node !== range.endContainer) {
-          if (node.nodeType === 1) {
-            reverse(node as HTMLElement, range, doc, tag);
-          } else if (node.nodeType === 3) {
-            const range = doc.createRange();
-            range.selectNode(node);
-            range.surroundContents(document.createElement(tag));
-          }
-          return;
-        }
-        if (node === range.startContainer && range.startOffset > 0) {
-          const startRange = doc.createRange();
-          startRange.setStartBefore(node);
-          startRange.setEnd(node, range.startOffset);
-          startRange.surroundContents(document.createElement(tag));
-          startRange.detach();
-        }
-        if (node === range.endContainer) {
-          const endRange = doc.createRange();
-          endRange.setStart(node, range.endOffset);
-          endRange.setEndAfter(node);
-          if (!endRange.collapsed) {
-            endRange.surroundContents(document.createElement(tag));
-          }
-          endRange.detach();
-        }
-
-      });
-    }
-
     const selection = this.contentDocument.getSelection();
     const range = selection.getRangeAt(0);
 
-    const shadowSelf = this.findParentByTagName(range.commonAncestorContainer as HTMLElement, tag) as HTMLElement;
-    if (shadowSelf) {
-      reverse(shadowSelf, range, this.contentDocument, tag);
-      this.takeOffWrapper(shadowSelf);
+    if (range.commonAncestorContainer.nodeType === 1 &&
+      (range.commonAncestorContainer as HTMLElement).tagName.toLowerCase() === tag) {
+      this.takeOffWrapper(range.commonAncestorContainer as HTMLElement);
+      return;
+    }
+
+    const parentTagContainer = this.matchContainerByTagName(range.commonAncestorContainer as HTMLElement, tag);
+    if (parentTagContainer) {
+      this.reverse(parentTagContainer, range, this.contentDocument, tag);
+      this.takeOffWrapper(parentTagContainer);
     } else {
       if (range.commonAncestorContainer.nodeType === 3) {
-        range.surroundContents(document.createElement(tag));
+        const newWrap = document.createElement(tag);
+        const isCollapsed = range.collapsed;
+        range.surroundContents(newWrap);
+        if (isCollapsed) {
+          newWrap.innerHTML = ' ';
+          range.selectNodeContents(newWrap);
+        }
       } else if (range.commonAncestorContainer.nodeType === 1) {
         if (this.hasOtherTag(range.cloneContents(), tag)) {
-          wrapper(range, selection, this.contentDocument, tag);
+          this.wrapper(range, this.contentDocument, tag);
         } else {
-          // 这里没好
-          // reverse(range.commonAncestorContainer as HTMLElement, range, this.contentDocument, tag);
-          // Array.from((range.commonAncestorContainer as HTMLElement).getElementsByTagName(tag)).forEach(item => {
-          //   this.takeOffWrapper(item);
-          // });
+          const startWrap = this.matchContainerByTagName(range.startContainer as HTMLElement, tag);
+          const endWrap = this.matchContainerByTagName(range.endContainer as HTMLElement, tag);
+          Array.from((range.commonAncestorContainer as HTMLElement).getElementsByTagName(tag))
+            .filter(item => item !== startWrap && item !== endWrap)
+            .forEach(node => {
+              this.takeOffWrapper(node);
+            });
+          const startRange = this.contentDocument.createRange();
+          startRange.setStart(range.startContainer, range.startOffset);
+          startRange.setEndAfter(startWrap);
+          const extractStart = startRange.extractContents().children[0];
+          startWrap.parentNode.insertBefore(extractStart, startWrap.nextSibling);
+          this.takeOffWrapper(extractStart);
+          startRange.detach();
+
+          const endRange = this.contentDocument.createRange();
+          endRange.setStartBefore(endWrap);
+          endRange.setEnd(range.endContainer, range.endOffset);
+          const extractEnd = endRange.extractContents().children[0];
+          endWrap.parentNode.insertBefore(extractEnd, endWrap);
+          this.takeOffWrapper(extractEnd);
+          endRange.detach();
         }
       }
     }
     this.contentDocument.body.focus();
+  }
+
+  private wrapper(range: Range, doc: Document, tag: string) {
+    if (range.startContainer === range.endContainer && range.commonAncestorContainer.nodeType === 3) {
+      range.surroundContents(document.createElement(tag));
+    } else {
+      this.getTextNodes(range.commonAncestorContainer as HTMLElement, tag)
+        .filter(item => range.intersectsNode(item))
+        .forEach(item => {
+          if (item.textContent === '') {
+            item.parentNode.removeChild(item);
+            return;
+          }
+          const temporaryRange = doc.createRange();
+          temporaryRange.selectNode(item);
+          if (item === range.startContainer) {
+            temporaryRange.setStart(item, range.startOffset);
+          } else if (item === range.endContainer) {
+            temporaryRange.setEnd(item, range.endOffset);
+          }
+          temporaryRange.surroundContents(document.createElement(tag));
+          temporaryRange.detach();
+        });
+    }
+  }
+
+  private reverse(container: HTMLElement, range: Range, doc: Document, tag: string) {
+    Array.from(container.childNodes).forEach(node => {
+      if (range.intersectsNode(node)) {
+        if (node.nodeType === 1) {
+          if ((node as HTMLElement).tagName.toLowerCase() === tag) {
+            this.takeOffWrapper(node as HTMLElement);
+          } else {
+            this.reverse(node as HTMLElement, range, doc, tag);
+          }
+        } else if (node.nodeType === 3) {
+          if (node === range.startContainer && range.startOffset > 0) {
+            const startRange = doc.createRange();
+            startRange.setStartBefore(node);
+            startRange.setEnd(node, range.startOffset);
+            startRange.surroundContents(document.createElement(tag));
+            startRange.detach();
+          }
+          if (node === range.endContainer) {
+            const endRange = doc.createRange();
+            endRange.setStart(node, range.endOffset);
+            endRange.setEndAfter(node);
+            if (!endRange.collapsed) {
+              endRange.surroundContents(document.createElement(tag));
+            }
+            endRange.detach();
+          }
+        }
+      } else {
+        if (node.nodeType === 1) {
+          this.reverse(node as HTMLElement, range, doc, tag);
+        } else if (node.nodeType === 3) {
+          const containerRange = doc.createRange();
+          containerRange.selectNode(node);
+          containerRange.surroundContents(document.createElement(tag));
+          containerRange.detach();
+        }
+      }
+    });
   }
 
   private getTextNodes(container: HTMLElement, exclude: string) {
@@ -176,7 +203,7 @@ export class Editor {
     return false;
   }
 
-  private findParentByTagName(node: Element, tagName: string) {
+  private matchContainerByTagName(node: HTMLElement, tagName: string) {
     while (node) {
       if (node.nodeType === 1 && node.tagName.toLowerCase() === tagName) {
         return node;
