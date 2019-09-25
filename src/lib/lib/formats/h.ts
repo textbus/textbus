7236069e6134b2f8d066b22ddeaa342a65939e53
tanbo <464328895@qq.com>
2019-09-25 18:07:37 +0800

    feat:dropdownHandler设计完成

--- src/lib/lib/formats/h.ts
+++ src/lib/lib/formats/h.ts
@@ -7,32 +7,32 @@ export const hHandler: SelectHandlerOption = {
   options: [{
     label: '标题1',
     match: {tags: ['H1']},
-    classes: ['tanbo-editor-toolbar-dropdown-menu-item-h1'],
+    classes: ['tanbo-editor-toolbar-select-menu-item-h1'],
     execCommand: new BlockFormatter('h1')
   }, {
     label: '标题2',
     match: {tags: ['H2']},
-    classes: ['tanbo-editor-toolbar-dropdown-menu-item-h2'],
+    classes: ['tanbo-editor-toolbar-select-menu-item-h2'],
     execCommand: new BlockFormatter('h2')
   }, {
     label: '标题3',
     match: {tags: ['H3']},
-    classes: ['tanbo-editor-toolbar-dropdown-menu-item-h3'],
+    classes: ['tanbo-editor-toolbar-select-menu-item-h3'],
     execCommand: new BlockFormatter('h3')
   }, {
     label: '标题4',
     match: {tags: ['H4']},
-    classes: ['tanbo-editor-toolbar-dropdown-menu-item-h4'],
+    classes: ['tanbo-editor-toolbar-select-menu-item-h4'],
     execCommand: new BlockFormatter('h4')
   }, {
     label: '标题5',
     match: {tags: ['H5']},
-    classes: ['tanbo-editor-toolbar-dropdown-menu-item-h5'],
+    classes: ['tanbo-editor-toolbar-select-menu-item-h5'],
     execCommand: new BlockFormatter('h5')
   }, {
     label: '标题6',
     match: {tags: ['H6']},
-    classes: ['tanbo-editor-toolbar-dropdown-menu-item-h6'],
+    classes: ['tanbo-editor-toolbar-select-menu-item-h6'],
     execCommand: new BlockFormatter('h6')
   }, {
     label: '正文',
