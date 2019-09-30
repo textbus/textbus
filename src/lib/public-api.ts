48b730c6101568aa69a282d74cc8c43b6923c106
tanbo <464328895@qq.com>
2019-09-30 20:32:12 +0800

    feat:导出成员类

--- src/lib/public-api.ts
+++ src/lib/public-api.ts
@@ -1,7 +1,7 @@
-export * from './lib/edit-frame/api';
-export * from './lib/formats/api';
+export * from './lib/edit-frame/_api';
+export * from './lib/formats/_api';
 export * from './lib/paths/paths';
-export * from './lib/toolbar/api';
+export * from './lib/toolbar/_api';
 export { Editor } from './lib/editor';
 export { MatchStatus, Matcher } from './lib/matcher';
 export { TBRange } from './lib/range';
