0b4866b1007e822f58d5d80fab79b65040d32cd3
tanbo <464328895@qq.com>
2019-11-22 17:35:44 +0800

    fear: 清除格式化功能完成

--- src/lib/lib/commands/clean-commander.ts
+++ src/lib/lib/commands/clean-commander.ts
@@ -1,35 +1,43 @@
 import { Commander } from './commander';
 import { TBSelection } from '../selection/selection';
 import { Handler } from '../toolbar/handlers/help';
-import { AttrState } from '../toolbar/formats/forms/help';
 import { Priority } from '../toolbar/help';
-import { FormatRange } from '../parser/fragment';
+import { FormatRange, Fragment } from '../parser/fragment';
 import { FormatState } from '../matcher/matcher';
 
-export class CleanCommander implements Commander<AttrState[]> {
+export class CleanCommander implements Commander<void> {
   recordHistory = true;
 
   command(selection: TBSelection, handler: Handler, overlap: boolean): void {
     selection.ranges.forEach(range => {
       range.getSelectedScope().forEach(item => {
-        const formatMatrix = item.context.formatMatrix;
-        Array.from(formatMatrix.keys()).filter(handler => {
-          return ![Priority.Default, Priority.Block, Priority.BlockStyle].includes(handler.priority);
-        }).forEach(handler => {
-          item.context.apply(new FormatRange({
-            state: FormatState.Invalid,
-            startIndex: item.startIndex,
-            endIndex: item.endIndex,
-            handler: handler,
-            context: item.context,
-            cacheData: null
-          }), false);
-        });
+        this.clean(item.context, item.startIndex, item.endIndex);
       });
     });
   }
 
   render(): null {
     return null;
+  }
+
+  private clean(fragment: Fragment, startIndex: number, endIndex: number) {
+    const formatMatrix = fragment.formatMatrix;
+    fragment.contents.slice(startIndex, endIndex).forEach(item => {
+      if (item instanceof Fragment) {
+        this.clean(item, 0, item.contents.length);
+      }
+    });
+    Array.from(formatMatrix.keys()).filter(handler => {
+      return ![Priority.Default, Priority.Block, Priority.BlockStyle].includes(handler.priority);
+    }).forEach(handler => {
+      fragment.apply(new FormatRange({
+        state: FormatState.Invalid,
+        startIndex,
+        endIndex,
+        handler: handler,
+        context: fragment,
+        cacheData: null
+      }), false);
+    });
   }
 }
