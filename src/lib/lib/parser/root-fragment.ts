380b27c984b688da9126dbed2dd79f6b9afe4af7
tanbo <464328895@qq.com>
2019-12-18 18:11:52 +0800

    feat:确保文档最后有一个空段落

--- src/lib/lib/parser/root-fragment.ts
+++ src/lib/lib/parser/root-fragment.ts
@@ -2,6 +2,10 @@ import { Fragment } from './fragment';
 import { Editor } from '../editor';
 import { Parser } from './parser';
 import { VIRTUAL_NODE } from './help';
+import { defaultHandlersMap } from '../default-handlers';
+import { Single } from './single';
+import { FormatRange } from './format';
+import { FormatState } from '../matcher/matcher';
 
 export class RootFragment extends Fragment {
   constructor(private parser: Parser, public editor: Editor) {
@@ -13,6 +17,26 @@ export class RootFragment extends Fragment {
   }
 
   render(host: HTMLElement, nextSibling?: Node): HTMLElement {
+    const last = this.contents.getContentAtIndex(this.contents.length - 1);
+    const newFragment = new Fragment(this);
+    newFragment.append(new Single(newFragment, 'br'));
+    newFragment.mergeFormat(new FormatRange({
+      state: FormatState.Valid,
+      startIndex: 0,
+      endIndex: 1,
+      handler: defaultHandlersMap.get('p'),
+      context: newFragment,
+      cacheData: {
+        tag: 'p'
+      }
+    }));
+    if (last instanceof Fragment) {
+      if (!last.formatMatrix.get(defaultHandlersMap.get('p'))) {
+        this.append(newFragment);
+      }
+    } else {
+      this.append(newFragment);
+    }
     const dom = super.render(host, nextSibling);
     host[VIRTUAL_NODE] = this.virtualNode;
     return dom;
