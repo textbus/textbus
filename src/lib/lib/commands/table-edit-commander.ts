634a1a4590a36cab2b417929abb2273ebcf9bdb5
tanbo <464328895@qq.com>
2019-12-18 17:11:47 +0800

    feat:表格删除行、列完成

--- src/lib/lib/commands/table-edit-commander.ts
+++ src/lib/lib/commands/table-edit-commander.ts
@@ -90,10 +90,8 @@ export class TableEditCommander implements Commander<TableEditParams> {
         break;
       case TableEditActions.SplitCells:
         const fragments = this.splitCells();
-        console.log(fragments)
         const firstRange = selection.firstRange;
         selection.removeAllRanges();
-
         fragments.forEach(f => {
           const range = firstRange.clone();
           range.startIndex = 0;
@@ -289,26 +287,152 @@ export class TableEditCommander implements Commander<TableEditParams> {
   }
 
   private deleteTopRow() {
+    const cellMatrix = this.params.cellMatrix;
+    const index = this.params.startPosition.rowIndex;
+
+    if (index === 0) {
+      return;
+    }
+    const prevRow = cellMatrix[index - 1];
+    const prevRowFragment = (prevRow.rowElement[VIRTUAL_NODE] as VirtualNode).context;
+    prevRow.cells.forEach((cell, cellIndex) => {
+      if (cell.columnOffset === 0) {
+        if (cell.rowOffset === 0 && cell.cellElement.rowSpan > 1) {
+          const rowFragment = (cell.rowElement[VIRTUAL_NODE] as VirtualNode).context;
+          const newCellFragment = TableEditCommander.createCell('td',
+            rowFragment,
+            cell.cellElement.rowSpan - 1,
+            cell.cellElement.colSpan);
+          const newPosition = cellMatrix[index].cells[cellIndex];
+          if (newPosition.afterCell) {
+            const index = (newPosition.afterCell[VIRTUAL_NODE] as VirtualNode).context.getIndexInParent();
+            rowFragment.insert(newCellFragment, index);
+          } else {
+            rowFragment.insert(newCellFragment, rowFragment.contents.length);
+          }
+        } else {
+          const formatRange = (cell.cellElement[VIRTUAL_NODE] as VirtualNode)
+            .context.formatMatrix.get(defaultHandlersMap.get('td'))[0];
+          formatRange.cacheData.attrs.set('rowspan', cell.cellElement.rowSpan - 1 + '');
+        }
+      }
+    });
+    prevRowFragment.parent.delete(prevRowFragment.getIndexInParent(), 1);
+    this.params.startPosition.rowIndex--;
+    this.params.endPosition.rowIndex--;
   }
 
   private deleteBottomRow() {
+    const cellMatrix = this.params.cellMatrix;
+    const index = this.params.endPosition.rowIndex;
+    if (index === cellMatrix.length - 1) {
+      return;
+    }
+    const nextRow = cellMatrix[index + 1];
+    const nextRowFragment = (nextRow.rowElement[VIRTUAL_NODE] as VirtualNode).context;
+    nextRow.cells.forEach((cell, cellIndex) => {
+      if (cell.columnOffset === 0) {
+        if (cell.rowOffset > 0) {
+          const formatRange = (cell.cellElement[VIRTUAL_NODE] as VirtualNode)
+            .context.formatMatrix.get(defaultHandlersMap.get('td'))[0];
+          formatRange.cacheData.attrs.set('rowspan', cell.cellElement.rowSpan - 1 + '');
+        } else if (cell.rowOffset === 0) {
+          if (cell.cellElement.rowSpan > 1) {
+
+            const newPosition = cellMatrix[index + 2].cells[cellIndex];
+            const rowFragment = (newPosition.rowElement[VIRTUAL_NODE] as VirtualNode).context;
+            const newCellFragment = TableEditCommander.createCell('td',
+              rowFragment,
+              cell.cellElement.rowSpan - 1,
+              cell.cellElement.colSpan);
+
+            if (newPosition.afterCell) {
+              const index = (newPosition.afterCell[VIRTUAL_NODE] as VirtualNode).context.getIndexInParent();
+              rowFragment.insert(newCellFragment, index);
+            } else {
+              rowFragment.insert(newCellFragment, rowFragment.contents.length);
+            }
+          }
+        }
+      }
+    });
+    nextRowFragment.parent.delete(nextRowFragment.getIndexInParent(), 1);
   }
 
   private deleteLeftColumn() {
+    const cellMatrix = this.params.cellMatrix;
+    const index = this.params.startPosition.columnIndex;
+
+    if (index === 0) {
+      return;
+    }
+    cellMatrix.forEach(row => {
+      const cell = row.cells[index - 1];
+      if (cell.rowOffset === 0) {
+        if (cell.columnOffset > 0) {
+          const formatRange = (cell.cellElement[VIRTUAL_NODE] as VirtualNode).context.formatMatrix.get(defaultHandlersMap.get('td'))[0];
+          formatRange.cacheData.attrs.set('colspan', cell.cellElement.colSpan - 1 + '');
+        } else {
+          const rowFragment = (cell.rowElement[VIRTUAL_NODE] as VirtualNode).context;
+          const index = (cell.cellElement[VIRTUAL_NODE] as VirtualNode).context.getIndexInParent();
+          if (cell.cellElement.colSpan > 1) {
+            const newCellFragment = TableEditCommander.createCell('td', rowFragment, cell.cellElement.rowSpan, cell.cellElement.colSpan - 1);
+            rowFragment.delete(index, 1);
+            rowFragment.insert(newCellFragment, index);
+          } else {
+            rowFragment.delete(index, 1);
+          }
+        }
+      }
+    });
+    this.params.startPosition.columnIndex--;
+    this.params.endPosition.columnIndex--;
   }
 
   private deleteRightColumn() {
+    const cellMatrix = this.params.cellMatrix;
+    const index = this.params.endPosition.columnIndex;
+    if (index === cellMatrix[0].cells.length - 1) {
+      return;
+    }
+    cellMatrix.forEach(row => {
+      const cell = row.cells[index + 1];
+      if (cell.rowOffset === 0) {
+        if (cell.columnOffset > 0) {
+          const formatRange = (cell.cellElement[VIRTUAL_NODE] as VirtualNode).context.formatMatrix.get(defaultHandlersMap.get('td'))[0];
+          formatRange.cacheData.attrs.set('colspan', cell.cellElement.colSpan - 1 + '');
+        } else {
+          const rowFragment = (cell.rowElement[VIRTUAL_NODE] as VirtualNode).context;
+          const index = (cell.cellElement[VIRTUAL_NODE] as VirtualNode).context.getIndexInParent();
+          if (cell.cellElement.colSpan > 1) {
+            const newCellFragment = TableEditCommander.createCell('td', rowFragment, cell.cellElement.rowSpan, cell.cellElement.colSpan - 1);
+            rowFragment.delete(index, 1);
+            rowFragment.insert(newCellFragment, index);
+          } else {
+            rowFragment.delete(index, 1);
+          }
+        }
+      }
+    });
   }
 
-  private static createCell(tagName: string, parent: Fragment) {
+  private static createCell(tagName: string, parent: Fragment, rowspan?: number, colspan?: number) {
     const cell = new Fragment(parent);
     cell.append(new Single(cell, 'br'));
+    const attrs = new Map<string, string>();
+    if (rowspan) {
+      attrs.set('rowspan', rowspan + '');
+    }
+    if (colspan) {
+      attrs.set('colspan', colspan + '');
+    }
     cell.mergeFormat(new FormatRange({
       state: FormatState.Valid,
       startIndex: 0,
       endIndex: 0,
       cacheData: {
-        tag: tagName
+        tag: tagName,
+        attrs
       },
       context: cell,
       handler: defaultHandlersMap.get(tagName)
