444db7eeabf31cc4cd2a72acada8cf1462746863
tanbo <464328895@qq.com>
2020-01-08 17:09:14 +0800

    feat: 引用功能完成

--- src/lib/lib/commands/toggle-block-commander.ts
+++ src/lib/lib/commands/toggle-block-commander.ts
@@ -3,7 +3,10 @@ import { FormatState } from '../matcher/matcher';
 import { TBSelection } from '../viewer/selection';
 import { Handler } from '../toolbar/handlers/help';
 import { Fragment } from '../parser/fragment';
-import { unwrap, wrap } from './utils';
+import { VIRTUAL_NODE } from '../parser/help';
+import { VNode } from '../renderer/virtual-dom';
+import { RootFragment } from '../parser/root-fragment';
+import { CacheData } from '../toolbar/utils/cache-data';
 
 export class ToggleBlockCommander implements Commander {
   recordHistory = true;
@@ -11,12 +14,57 @@ export class ToggleBlockCommander implements Commander {
   constructor(private tagName: string) {
   }
 
-  command(selection: TBSelection, handler: Handler, overlap: boolean): Fragment {
+  command(selection: TBSelection, handler: Handler, overlap: boolean, rootFragment: RootFragment): void {
     if (overlap) {
-      return unwrap(selection, handler);
+      const reg = new RegExp(this.tagName, 'i');
+      selection.ranges.forEach(range => {
+        const commonAncestorFragment = range.commonAncestorFragment;
+        let node = commonAncestorFragment.vNode.nativeElement;
+        let container: Fragment;
+        while (node) {
+          if (reg.test(node.nodeName)) {
+            container = (node[VIRTUAL_NODE] as VNode).context;
+            break;
+          }
+          node = node.parentNode;
+        }
+        if (container) {
+          // 在一个 block 内
+          const index = container.getIndexInParent();
+          container.parent.insertFragmentContents(container, index);
+          container.destroy();
+        } else {
+          // 选中多个 block
+          const scope = range.getCommonAncestorFragmentScope();
+          const temporaryFragment = new Fragment();
+          range.getBlockFragmentsBySelectedScope().forEach(item => {
+            const node = item.context.vNode.nativeElement;
+            if (reg.test(node.parentNode.nodeName)) {
+              temporaryFragment.insertFragmentContents(item.context, temporaryFragment.contentLength);
+              this.deleteEmptyFragment(item.context, commonAncestorFragment);
+            } else {
+              const p = item.context.parent;
+              temporaryFragment.append(item.context);
+              this.deleteEmptyFragment(p, commonAncestorFragment);
+            }
+          });
+          commonAncestorFragment.insertFragmentContents(temporaryFragment, scope.startIndex);
+        }
+      });
     } else {
-      unwrap(selection, handler);
-      return wrap(selection, handler, this.tagName);
+      selection.ranges.forEach(range => {
+        const commonAncestorFragment = range.commonAncestorFragment;
+        const temporaryFragment = new Fragment(rootFragment.parser.getFormatStateByData(new CacheData({
+          tag: this.tagName
+        })));
+        const index = range.getCommonAncestorFragmentScope().startIndex;
+        range.getBlockFragmentsBySelectedScope().forEach(item => {
+          const parent = item.context.parent;
+          temporaryFragment.append(item.context);
+          this.deleteEmptyFragment(parent, commonAncestorFragment);
+        });
+        commonAncestorFragment.insert(temporaryFragment, index);
+      });
     }
   }
 
@@ -25,5 +73,16 @@ export class ToggleBlockCommander implements Commander {
       return new ReplaceModel(document.createElement(this.tagName));
     }
     return null;
+  }
+
+  private deleteEmptyFragment(fragment: Fragment, scope: Fragment) {
+    if (!fragment || fragment === scope) {
+      return;
+    }
+    const parent = fragment.parent;
+    if (fragment.contentLength === 0) {
+      fragment.destroy();
+    }
+    this.deleteEmptyFragment(parent, scope);
   }
 }
