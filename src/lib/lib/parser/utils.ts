155b4f2ac46002139c96111def93eb1aedc1e325
tanbo <464328895@qq.com>
2020-01-21 10:58:18 +0800

    fix: 修复插入零宽 formatRange 时，不要插入同类 format 的问题

--- src/lib/lib/parser/utils.ts
+++ src/lib/lib/parser/utils.ts
@@ -57,56 +57,66 @@ export function mergeFormat(formatMap: Map<Handler, FormatRange[]>, format: Form
   }) as InlineFormat[];
 
   if (oldFormats) {
-    const formatMarks: Array<InlineFormat> = [];
-    if (important) {
-      oldFormats.unshift(format);
+    if (format.startIndex === format.endIndex) {
+      formatRanges.push(...oldFormats);
+      const flag = oldFormats.filter(f => {
+        return f.startIndex <= format.startIndex && f.endIndex >= format.endIndex;
+      }).length > 0;
+      if (!flag) {
+        formatRanges.push(format);
+      }
     } else {
-      oldFormats.push(format);
-    }
-    let index = oldFormats.length - 1;
-    while (index >= 0) {
-      const item = oldFormats[index];
-      if (formatMarks.length < item.endIndex) {
-        formatMarks.length = item.endIndex;
-      }
-      formatMarks.fill(item, item.startIndex, item.endIndex);
-      index--;
-    }
-    let newFormatRange: InlineFormat = null;
-    for (let i = 0; i < formatMarks.length; i++) {
-      const mark = formatMarks[i];
-
-      if (!mark) {
-        newFormatRange = null;
-        continue;
-      }
-      if (!newFormatRange) {
-        newFormatRange = new InlineFormat({
-          startIndex: i,
-          endIndex: i + 1,
-          handler: mark.handler,
-          context: mark.context,
-          state: mark.state,
-          abstractData: mark.abstractData
-        });
-        formatRanges.push(newFormatRange);
-        continue;
-      }
-      if (mark.state === newFormatRange.state && (
-        mark.abstractData &&
-        newFormatRange.abstractData &&
-        mark.abstractData.equal(newFormatRange.abstractData) || !mark.abstractData === true && !newFormatRange.abstractData === true)) {
-        newFormatRange.endIndex = i + 1;
+      const formatMarks: Array<InlineFormat> = [];
+      if (important) {
+        oldFormats.unshift(format);
       } else {
-        newFormatRange = new InlineFormat({
-          startIndex: i,
-          endIndex: i + 1,
-          handler: mark.handler,
-          context: mark.context,
-          state: mark.state,
-          abstractData: mark.abstractData
-        });
-        formatRanges.push(newFormatRange);
+        oldFormats.push(format);
+      }
+      let index = oldFormats.length - 1;
+      while (index >= 0) {
+        const item = oldFormats[index];
+        if (formatMarks.length < item.endIndex) {
+          formatMarks.length = item.endIndex;
+        }
+        formatMarks.fill(item, item.startIndex, item.endIndex);
+        index--;
+      }
+      let newFormatRange: InlineFormat = null;
+      for (let i = 0; i < formatMarks.length; i++) {
+        const mark = formatMarks[i];
+
+        if (!mark) {
+          newFormatRange = null;
+          continue;
+        }
+        if (!newFormatRange) {
+          newFormatRange = new InlineFormat({
+            startIndex: i,
+            endIndex: i + 1,
+            handler: mark.handler,
+            context: mark.context,
+            state: mark.state,
+            abstractData: mark.abstractData
+          });
+          formatRanges.push(newFormatRange);
+          continue;
+        }
+        if (mark.state === newFormatRange.state && (
+          mark.abstractData &&
+          newFormatRange.abstractData &&
+          mark.abstractData.equal(newFormatRange.abstractData) || !mark.abstractData === true && !newFormatRange.abstractData === true)) {
+          newFormatRange.endIndex = i + 1;
+        } else {
+          newFormatRange = new InlineFormat({
+            startIndex: i,
+            endIndex: i + 1,
+            handler: mark.handler,
+            context: mark.context,
+            state: mark.state,
+            abstractData: mark.abstractData
+          });
+          formatRanges.push(newFormatRange);
+        }
       }
     }
   } else {
--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ const editor = createEditor('#editor', {
     console.log(type);
     return '/test';
   },
-  content: `323223`
+  content: `1111<em>323223</em>5454`
 });
 
 // editor.updateContentHTML('<p>p1<span>p-span</span></p><span>span3</span><span>span4</span><p>p2</p><span>span1</span><span>span2</span>')
