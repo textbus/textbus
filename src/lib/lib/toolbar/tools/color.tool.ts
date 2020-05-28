fc6749bebe0e9081cfb87c34b5a76271a9d81fe8
tanbo <464328895@qq.com>
2020-05-28 16:57:26 +0800

    feat: 增加 block format 支持

--- src/lib/lib/toolbar/tools/color.tool.ts
+++ src/lib/lib/toolbar/tools/color.tool.ts
@@ -1,47 +1,19 @@
-import { createPicker, Picker } from '@tanbo/color-picker';
-import { ColorHSL, ColorRGB, hsl2Hex, parseCss, rgb2Hex } from '@tanbo/color';
 import { Subject } from 'rxjs';
 
 import { DropdownConfig, HandlerType } from '../help';
 import { colorFormatter } from '../../formatter/style.formatter';
-import { DropdownHandlerView } from '../handlers/utils/dropdown';
 import { StyleCommander } from '../commands/style.commander';
-import { FormatAbstractData } from '../../core/format-abstract-data';
 import { FormatMatcher } from '../matcher/format.matcher';
+import { Palette } from './utils/palette';
 
 const hideEvent = new Subject<void>();
 const commander = new StyleCommander('color', colorFormatter);
-
-class Palette implements DropdownHandlerView {
-  elementRef = document.createElement('div');
-
-  private picker: Picker;
-
-  constructor() {
-    this.picker = createPicker(this.elementRef);
-    this.picker.onSelected = function (ev) {
-      commander.updateValue(ev.hex);
-      hideEvent.next();
-    };
-  }
-
-  update(d?: FormatAbstractData): void {
-    const color = d ? (d.style.value + '') : '#f00';
-    if (/^#/.test(color)) {
-      this.picker.hex = color;
-    } else if (/^rgba?/.test(color)) {
-      this.picker.hex = rgb2Hex((parseCss(color) as ColorRGB));
-    } else if (/^hsl/.test(color)) {
-      this.picker.hex = hsl2Hex((parseCss(color) as ColorHSL));
-    }
-  }
-}
 
 export const colorTool: DropdownConfig = {
   type: HandlerType.Dropdown,
   classes: ['tbus-icon-color'],
   tooltip: '文字颜色',
-  viewer: new Palette(),
+  viewer: new Palette(commander, hideEvent),
   onHide: hideEvent.asObservable(),
   match: new FormatMatcher(colorFormatter),
   execCommand: commander
---
+++ src/lib/lib/toolbar/tools/utils/palette.ts
@@ -0,0 +1,32 @@
+import { createPicker, Picker } from '@tanbo/color-picker';
+import { ColorHSL, ColorRGB, hsl2Hex, parseCss, rgb2Hex } from '@tanbo/color';
+import { Subject } from 'rxjs';
+
+import { DropdownHandlerView } from '../../handlers/utils/dropdown';
+import { FormatAbstractData } from '../../../core/format-abstract-data';
+import { Commander } from '../../commands/commander';
+
+export class Palette implements DropdownHandlerView {
+  elementRef = document.createElement('div');
+
+  private picker: Picker;
+
+  constructor(commander: Commander, hideEvent: Subject<any>) {
+    this.picker = createPicker(this.elementRef);
+    this.picker.onSelected = function (ev) {
+      commander.updateValue(ev.hex);
+      hideEvent.next();
+    };
+  }
+
+  update(d?: FormatAbstractData): void {
+    const color = d ? (d.style.value + '') : '#f00';
+    if (/^#/.test(color)) {
+      this.picker.hex = color;
+    } else if (/^rgba?/.test(color)) {
+      this.picker.hex = rgb2Hex((parseCss(color) as ColorRGB));
+    } else if (/^hsl/.test(color)) {
+      this.picker.hex = hsl2Hex((parseCss(color) as ColorHSL));
+    }
+  }
+}
