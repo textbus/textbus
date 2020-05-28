b68c858ed69c8ee8f255fca9ba5131016212dc09
tanbo <464328895@qq.com>
2020-05-28 18:19:25 +0800

    feat: 块模板回车行为开发

--- src/lib/lib/templates/block.template.ts
+++ src/lib/lib/templates/block.template.ts
@@ -1,6 +1,8 @@
 import { Template, TemplateTranslator, ViewData } from '../core/template';
 import { Fragment } from '../core/fragment';
 import { VElement } from '../core/element';
+import { EventType } from '../core/events';
+import { SingleTemplate } from './single.template';
 
 export class BlockTemplateTranslator implements TemplateTranslator {
   constructor(private tagName: string) {
@@ -32,6 +34,29 @@ export class BlockTemplate extends Template {
   render() {
     const block = new VElement(this.tagName);
     this.viewMap.set(this.childSlots[0], block);
+    block.events.subscribe(event => {
+      if (event.type === EventType.onEnter) {
+        const parent = event.renderer.getParentFragmentByTemplate(this);
+
+        const template = new BlockTemplate(this.tagName);
+        const fragment = new Fragment();
+        template.childSlots.push(fragment);
+        const firstRange = event.selection.firstRange;
+        const c = firstRange.startFragment.delete(firstRange.startIndex);
+        if (firstRange.startFragment.contentLength === 0) {
+          firstRange.startFragment.append(new SingleTemplate('br'));
+        }
+        if (c.contents.length) {
+          c.contents.forEach(cc => fragment.append(cc));
+        } else {
+          fragment.append(new SingleTemplate('br'));
+        }
+        c.formatRanges.forEach(ff => fragment.mergeFormat(ff));
+        parent.insert(template, parent.find(this) + 1);
+        firstRange.startFragment = firstRange.endFragment = fragment
+        firstRange.startIndex = firstRange.endIndex = 0;
+      }
+    })
     return block;
   }
 }
