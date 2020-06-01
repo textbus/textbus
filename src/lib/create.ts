c9ff9a771aeacbd247f7dab4d85ab68caa5c3c63
tanbo <464328895@qq.com>
2020-06-01 20:51:08 +0800

    feat: 发布测试包

---
+++ src/lib/create.ts
@@ -0,0 +1,115 @@
+import { EditorOptions, Editor } from './lib/editor';
+import {
+  fontFamilyFormatter,
+  boldFormatter,
+  linkFormatter,
+  backgroundColor,
+  colorFormatter,
+  fontSizeFormatter,
+  italicFormatter,
+  letterSpacingFormatter,
+  lineHeightFormatter,
+  strikeThroughFormatter,
+  subscriptFormatter,
+  superscriptFormatter,
+  textAlignFormatter,
+  textIndentFormatter,
+  underlineFormatter
+} from './lib/formatter/_api';
+import {
+  audioTool,
+  blockBackgroundTool,
+  boldTool,
+  cleanTool,
+  codeTool,
+  colorTool,
+  emojiTool,
+  fontFamilyTool,
+  fontSizeTool,
+  headingTool,
+  historyBackTool,
+  historyForwardTool,
+  imageTool,
+  italicTool,
+  letterSpacingTool,
+  lineHeightTool,
+  linkTool,
+  olTool,
+  strikeThroughTool,
+  subscriptTool,
+  superscriptTool,
+  tableEditTool,
+  tableTool,
+  textAlignTool,
+  textBackgroundTool,
+  textIndentTool,
+  ulTool,
+  underlineTool,
+  videoTool
+} from './lib/toolbar/tools/_api';
+import { DefaultHook, HistoryHook } from './lib/hooks/_api';
+import {
+  AudioTemplateTranslator,
+  BlockTemplateTranslator,
+  CodeTemplateTranslator,
+  ImageTemplateTranslator,
+  ListTemplateTranslator,
+  SingleTemplateTranslator,
+  TableTemplateTranslator,
+  VideoTemplateTranslator
+} from './lib/templates/_api';
+import { defaultStyleSheets } from './lib/viewer/default-styles';
+
+export function createEditor(selector: string | HTMLElement, options: EditorOptions = {}) {
+  const defaultOptions: EditorOptions = {
+    styleSheets: defaultStyleSheets,
+    hooks: [
+      new DefaultHook(),
+      new HistoryHook()
+    ],
+    templateTranslators: [
+      new ListTemplateTranslator('ul'),
+      new ListTemplateTranslator('ol'),
+      new BlockTemplateTranslator('div,p,h1,h2,h3,h4,h5,h6'.split(',')),
+      new SingleTemplateTranslator('br'),
+      new CodeTemplateTranslator(),
+      new AudioTemplateTranslator(),
+      new VideoTemplateTranslator(),
+      new ImageTemplateTranslator(),
+      new TableTemplateTranslator()
+    ],
+    formatters: [
+      fontFamilyFormatter,
+      boldFormatter,
+      linkFormatter,
+      backgroundColor,
+      colorFormatter,
+      fontSizeFormatter,
+      italicFormatter,
+      letterSpacingFormatter,
+      lineHeightFormatter,
+      strikeThroughFormatter,
+      subscriptFormatter,
+      superscriptFormatter,
+      textAlignFormatter,
+      textIndentFormatter,
+      underlineFormatter
+    ],
+    toolbar: [
+      [historyBackTool, historyForwardTool],
+      [headingTool],
+      [boldTool, italicTool, strikeThroughTool, underlineTool],
+      [codeTool],
+      [olTool, ulTool],
+      [fontSizeTool, lineHeightTool, letterSpacingTool, textIndentTool],
+      [subscriptTool, superscriptTool],
+      [colorTool, textBackgroundTool, blockBackgroundTool, emojiTool],
+      [fontFamilyTool],
+      [linkTool, imageTool, audioTool, videoTool],
+      [textAlignTool],
+      [tableTool, tableEditTool],
+      [cleanTool]
+    ]
+  };
+  return new Editor(selector, Object.assign(defaultOptions, options));
+}
