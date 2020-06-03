5fa1036de45ef8ed23704a5bce94ab7ef3a8fb3c
tanbo <464328895@qq.com>
2020-06-03 15:54:17 +0800

    fix: 修复删除残留bug

--- src/lib/create.ts
+++ src/lib/create.ts
@@ -60,56 +60,57 @@ import {
 } from './lib/templates/_api';
 import { defaultStyleSheets } from './lib/viewer/default-styles';
 
+export const defaultOptions: EditorOptions = {
+  styleSheets: defaultStyleSheets,
+  hooks: [
+    new DefaultHook(),
+    new HistoryHook()
+  ],
+  templateTranslators: [
+    new ListTemplateTranslator('ul'),
+    new ListTemplateTranslator('ol'),
+    new BlockTemplateTranslator('div,p,h1,h2,h3,h4,h5,h6'.split(',')),
+    new SingleTemplateTranslator('br'),
+    new CodeTemplateTranslator(),
+    new AudioTemplateTranslator(),
+    new VideoTemplateTranslator(),
+    new ImageTemplateTranslator(),
+    new TableTemplateTranslator()
+  ],
+  formatters: [
+    fontFamilyFormatter,
+    boldFormatter,
+    linkFormatter,
+    backgroundColor,
+    colorFormatter,
+    fontSizeFormatter,
+    italicFormatter,
+    letterSpacingFormatter,
+    lineHeightFormatter,
+    strikeThroughFormatter,
+    subscriptFormatter,
+    superscriptFormatter,
+    textAlignFormatter,
+    textIndentFormatter,
+    underlineFormatter
+  ],
+  toolbar: [
+    [historyBackTool, historyForwardTool],
+    [headingTool],
+    [boldTool, italicTool, strikeThroughTool, underlineTool],
+    [codeTool],
+    [olTool, ulTool],
+    [fontSizeTool, lineHeightTool, letterSpacingTool, textIndentTool],
+    [subscriptTool, superscriptTool],
+    [colorTool, textBackgroundTool, blockBackgroundTool, emojiTool],
+    [fontFamilyTool],
+    [linkTool, imageTool, audioTool, videoTool],
+    [textAlignTool],
+    [tableTool, tableEditTool],
+    [cleanTool]
+  ]
+};
+
 export function createEditor(selector: string | HTMLElement, options: EditorOptions = {}) {
-  const defaultOptions: EditorOptions = {
-    styleSheets: defaultStyleSheets,
-    hooks: [
-      new DefaultHook(),
-      new HistoryHook()
-    ],
-    templateTranslators: [
-      new ListTemplateTranslator('ul'),
-      new ListTemplateTranslator('ol'),
-      new BlockTemplateTranslator('div,p,h1,h2,h3,h4,h5,h6'.split(',')),
-      new SingleTemplateTranslator('br'),
-      new CodeTemplateTranslator(),
-      new AudioTemplateTranslator(),
-      new VideoTemplateTranslator(),
-      new ImageTemplateTranslator(),
-      new TableTemplateTranslator()
-    ],
-    formatters: [
-      fontFamilyFormatter,
-      boldFormatter,
-      linkFormatter,
-      backgroundColor,
-      colorFormatter,
-      fontSizeFormatter,
-      italicFormatter,
-      letterSpacingFormatter,
-      lineHeightFormatter,
-      strikeThroughFormatter,
-      subscriptFormatter,
-      superscriptFormatter,
-      textAlignFormatter,
-      textIndentFormatter,
-      underlineFormatter
-    ],
-    toolbar: [
-      [historyBackTool, historyForwardTool],
-      [headingTool],
-      [boldTool, italicTool, strikeThroughTool, underlineTool],
-      [codeTool],
-      [olTool, ulTool],
-      [fontSizeTool, lineHeightTool, letterSpacingTool, textIndentTool],
-      [subscriptTool, superscriptTool],
-      [colorTool, textBackgroundTool, blockBackgroundTool, emojiTool],
-      [fontFamilyTool],
-      [linkTool, imageTool, audioTool, videoTool],
-      [textAlignTool],
-      [tableTool, tableEditTool],
-      [cleanTool]
-    ]
-  };
   return new Editor(selector, Object.assign(defaultOptions, options));
 }
