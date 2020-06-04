b71414785383b3d82a28dc2f7fdea6cb1e5cb78e
tanbo <464328895@qq.com>
2020-06-04 23:00:53 +0800

    feat: 编辑文档注释中

--- src/lib/lib/editor.ts
+++ src/lib/lib/editor.ts
@@ -20,16 +20,23 @@ export interface Snapshot {
 }
 
 export interface EditorOptions {
+  /** 设置主题 */
   theme?: string;
+  /** 设置最大历史栈 */
   historyStackSize?: number;
+  /** 设置模板转换器 */
   templateTranslators?: TemplateTranslator[];
+  /** 设置格式转换器 */
   formatters?: InlineFormatter[];
+  /** 工具条配置 */
   toolbar?: (ToolFactory | ToolFactory[])[];
+  /** 配置生命周期勾子 */
   hooks?: Lifecycle[];
+  /** 配置编辑器的默认样式 */
   styleSheets?: string[];
-
+  /** 当某些工具需要上传资源时的调用函数，调用时会传入上传资源的类型，如 image、video、audio等，该函数返回一个字符串，作为资源的 url 地址 */
   uploader?(type: string): (string | Promise<string> | Observable<string>);
-
+  /** 设置初始化 TBus 时的默认内容 */
   contents?: string;
 }
 
