db115eae1427e8115816bae43bebfa145566336a
tanbo <464328895@qq.com>
2020-06-05 23:13:23 +0800

    feat: list 开发中。。。

--- src/lib/lib/toolbar/commands/list.commander.ts
+++ src/lib/lib/toolbar/commands/list.commander.ts
@@ -1,4 +1,5 @@
-import { Commander, TBSelection, Renderer } from '../../core/_api';
+import { Commander, TBSelection, Renderer, Fragment } from '../../core/_api';
+import { ListTemplate } from '../../templates/list.template';
 
 export class ListCommander implements Commander {
   recordHistory = true;
@@ -8,9 +9,23 @@ export class ListCommander implements Commander {
 
   command(selection: TBSelection, overlap: boolean, renderer: Renderer): void {
     selection.ranges.forEach(range => {
-      range.getBlockFragmentsBySelectedScope().forEach(scope => {
-        console.log(scope)
-      })
+      const commonScope = range.getCommonAncestorFragmentScope();
+      if (overlap) {
+
+      } else {
+        const list = new ListTemplate(this.tagName);
+        range.getSuccessiveContents().reverse().forEach(scope => {
+          const fragment = new Fragment();
+          const contents = scope.fragment.delete(scope.startIndex, scope.endIndex - scope.startIndex);
+          contents.contents.forEach(c => fragment.append(c));
+          contents.formatRanges.forEach(f => fragment.mergeFormat(f));
+          list.childSlots.push(fragment);
+          if (scope.fragment.contentLength === 0) {
+            range.deleteEmptyTree(scope.fragment, range.commonAncestorFragment);
+          }
+        });
+        range.commonAncestorFragment.insert(list, commonScope.startIndex);
+      }
     })
   }
 }
