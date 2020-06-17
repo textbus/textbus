c2fa3e36a8218227665d000d44b5348c54bf7e45
tanbo <464328895@qq.com>
2020-06-17 12:56:30 +0800

    fix: 修复清除格式时未完全清除所有选中范围的问题

--- src/lib/lib/toolbar/commands/clean.commander.ts
+++ src/lib/lib/toolbar/commands/clean.commander.ts
@@ -16,7 +16,7 @@ export class CleanCommander implements Commander {
 
   command(selection: TBSelection, overlap: boolean): void {
     selection.ranges.forEach(range => {
-      range.getSelectedScope().forEach(scope => {
+      range.getSuccessiveContents().forEach(scope => {
         scope.fragment.getFormatRanges().forEach(f => {
           if (f.startIndex > scope.endIndex ||
             f.endIndex < f.startIndex ||
