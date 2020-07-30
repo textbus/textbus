ae71b6a5fcdd3d2f47f5de79b271b20152c381d8
tanbo <464328895@qq.com>
2020-07-30 23:34:17 +0800

    feat: 查找替换功能基本完成

--- src/lib/lib/core/commander.ts
+++ src/lib/lib/core/commander.ts
@@ -13,10 +13,10 @@ export interface Commander<T = any> {
   /**
    * 格式化文档的方法
    * @param selection 当前用户操作的选区
-   * @param config 当前命令所需要的值
+   * @param params 执行当前命令所需要的参数，大多数情况下，由菜单栏工具生成，也可以通过生命周期生成。
    * @param overlap 根据当前选区和工具类的 `Matcher` 匹配出的结果得到的状态，`true` 为完全重叠，`false` 为不完全重叠或不重叠
    * @param renderer 渲染器
    * @param rootFragment 根编辑片段
    */
-  command(selection: TBSelection, config: T, overlap: boolean, renderer: Renderer, rootFragment: Fragment): void;
+  command(selection: TBSelection, params: T, overlap: boolean, renderer: Renderer, rootFragment: Fragment): void;
 }
