76df56c83aba3b6cc4f57d3c08873dc64711dd06
tanbo <464328895@qq.com>
2020-08-04 17:05:08 +0800

    refactor: 更改包名

--- src/lib/lib/viewer/iframe-html.ts
+++ src/lib/lib/viewer/iframe-html.ts
@@ -6,7 +6,7 @@ export const iframeHTML = `
   <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>@tanbo/TextBus</title>
+  <title>@tanbo/textbus</title>
   <style>
     html {
       height: 100%
