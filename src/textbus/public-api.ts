150351fd2209cdf6f140bcbc2fed989e3fc83651
tanbo <464328895@qq.com>
2020-09-15 14:41:02 +0800

    feat: 导出 ui 相关 api

--- src/textbus/public-api.ts
+++ src/textbus/public-api.ts
@@ -1,9 +1,12 @@
+export * from './lib/additional-components/_api';
+export * from './lib/components/_api';
 export * from './lib/core/_api';
 export * from './lib/formatter/_api';
 export * from './lib/hooks/_api';
-export * from './lib/components/_api';
 export * from './lib/toolbar/_api';
+export * from './lib/uikit/_api';
 export * from './lib/viewer/_api';
+export * from './lib/workbench/_api';
 
 export * from './lib/editor';
 export * from './lib/event-handler';
