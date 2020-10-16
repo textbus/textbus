f422c1d932dc674813913b589cf1c50b0f2b4ed3
tanbo <464328895@qq.com>
2020-10-16 21:26:14 +0800

    test: 优化单元测试

--- test/core/fragment.spec.ts
+++ test/core/fragment.spec.ts
@@ -1,32 +1,32 @@
 import { BoldFormatter, FormatAbstractData, FormatEffect, Fragment } from '@tanbo/textbus';
 
-const formatter = new BoldFormatter();
-
-const f = {
-  startIndex: 3,
-  endIndex: 6,
-  abstractData: new FormatAbstractData(),
-  state: FormatEffect.Valid
-};
-
-
 describe('删除内容', () => {
-  test('删除前面', () => {
-    const fragment = new Fragment();
+  let fragment: Fragment;
+  const formatter = new BoldFormatter();
+  const f = {
+    startIndex: 3,
+    endIndex: 6,
+    abstractData: new FormatAbstractData(),
+    state: FormatEffect.Valid
+  };
+
+  beforeEach(() => {
+    fragment = new Fragment();
     fragment.append('0123456789');
     fragment.apply(formatter, f);
+  })
+  test('删除前面', () => {
     const deletedContents = fragment.cut(1, 2);
     expect(fragment.getFormatRanges(formatter)[0]).toEqual({
       ...f,
       startIndex: 1,
       endIndex: 4,
     })
-    expect(deletedContents.getFormatKeys()).toEqual(0)
+    expect(fragment.sliceContents(0)[0]).toBe('03456789');
+    expect(deletedContents.sliceContents(0)[0]).toBe('12');
+    expect(deletedContents.getFormatKeys().length).toEqual(0);
   })
   test('删除前面加中间', () => {
-    const fragment = new Fragment();
-    fragment.append('0123456789');
-    fragment.apply(formatter, f);
     const deletedContents = fragment.cut(1, 4);
     expect(fragment.getFormatRanges(formatter)[0]).toEqual({
       ...f,
@@ -35,8 +35,8 @@ describe('删除内容', () => {
     })
     expect(deletedContents.getFormatRanges(formatter)[0]).toEqual({
       ...f,
-      startIndex: 1,
-      endIndex: 3
+      startIndex: 2,
+      endIndex: 4
     })
   });
 })
