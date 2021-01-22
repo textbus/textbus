cfbcdf055262b1de3726fada0b3c3cc912429be5
tanbo <464328895@qq.com>
2021-01-22 22:09:28 +0800

    feat: 优化命名

--- src/lib/toolbar/tools/insert-paragraph-after.tool.ts
+++ src/lib/toolbar/tools/insert-paragraph-after.tool.ts
@@ -1,11 +1,11 @@
 import { ButtonToolConfig, Toolkit } from '../toolkit/_api';
 import { InsertParagraphCommander } from '../commands/insert-paragraph.commander';
 
-export const insertParagraphAfterConfig: ButtonToolConfig = {
+export const insertParagraphAfterToolConfig: ButtonToolConfig = {
   iconClasses: ['textbus-icon-insert-paragraph-after'],
   tooltip: '在后面插入段落',
   commanderFactory() {
     return new InsertParagraphCommander(false);
   }
 };
-export const insertParagraphAfterTool = Toolkit.makeButtonTool(insertParagraphAfterConfig);
+export const insertParagraphAfterTool = Toolkit.makeButtonTool(insertParagraphAfterToolConfig);
--- src/lib/toolbar/tools/insert-paragraph-before.tool.ts
+++ src/lib/toolbar/tools/insert-paragraph-before.tool.ts
@@ -1,11 +1,11 @@
 import { ButtonToolConfig, Toolkit } from '../toolkit/_api';
 import { InsertParagraphCommander } from '../commands/insert-paragraph.commander';
 
-export const insertParagraphBeforeConfig: ButtonToolConfig = {
+export const insertParagraphBeforeToolConfig: ButtonToolConfig = {
   iconClasses: ['textbus-icon-insert-paragraph-before'],
   tooltip: '在前面插入段落',
   commanderFactory() {
     return new InsertParagraphCommander(true);
   }
 };
-export const insertParagraphBeforeTool = Toolkit.makeButtonTool(insertParagraphBeforeConfig);
+export const insertParagraphBeforeTool = Toolkit.makeButtonTool(insertParagraphBeforeToolConfig);
