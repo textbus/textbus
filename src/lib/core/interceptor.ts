aeac9055e0e2ae76e81b49913d314df5a175db59
tanbo <464328895@qq.com>
2021-02-01 22:28:31 +0800

    docs: 更新文档注释

--- src/lib/core/interceptor.ts
+++ src/lib/core/interceptor.ts
@@ -39,22 +39,53 @@ export class TBEvent<T extends AbstractComponent, U = null> {
 }
 
 /**
- * TextBus 生命周期方法。
+ * TextBus 组件编辑行为生命周期方法。
  */
 export abstract class Interceptor<T extends AbstractComponent> {
+  /**
+   * 在当前组件下输入功能就绪时调用
+   * @param event
+   */
   abstract onInputReady?(event: TBEvent<T>): void;
 
+  /**
+   * 在当前组件下，用户输入时调用
+   * @param event
+   */
   abstract onInput?(event: TBEvent<T>): void;
 
+  /**
+   * 在当前组件下，选区末闭合且触发删除行为时调用
+   * @param event
+   */
   abstract onDeleteRange?(event: TBEvent<T>): void;
 
+  /**
+   * 在当前组件下，选区闭合且触发删除行为时调用
+   * @param event
+   */
   abstract onDelete?(event: TBEvent<T>): void;
 
+  /**
+   * 在当前组件下，用户敲击回车键时调用
+   * @param event
+   */
   abstract onEnter?(event: TBEvent<T>): void;
 
+  /**
+   * 当在当前组件下，触发粘贴行为时调用
+   * @param event
+   */
   abstract onPaste?(event: TBEvent<T, TBClipboard>): void;
 
+  /**
+   * 在当前组件下，触发上下文菜单时调用，需返回上下文本菜单配置，TextBus 会根据配置生成上下文本菜单。
+   * @param instance
+   */
   abstract onContextmenu?(instance: T): ContextMenuAction[];
 
+  /**
+   * 当 TextBus 销毁时调用
+   */
   abstract onDestroy?(): void;
 }
