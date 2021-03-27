56e218fb8be93b06630e144903b1b2d2f048fc96
tanbo <464328895@qq.com>
2021-03-28 01:46:25 +0800

    feat: 添加国际化支持

--- src/public-api.ts
+++ src/public-api.ts
@@ -3,6 +3,7 @@ export * from 'reflect-metadata';
 export * from '@tanbo/di';
 
 export * from './lib/components/_api';
+export * from './lib/i18n/_api';
 export * from './lib/core/_api';
 export * from './lib/formatter/_api';
 export * from './lib/ui/_api';
@@ -12,6 +13,7 @@ export * from './lib/editor';
 export * from './lib/editor-controller';
 export * from './lib/editor-options';
 export * from './lib/history';
+export * from './lib/i18n';
 export * from './lib/inject-tokens';
 export * from './lib/output-translator';
 export * from './lib/root-component';
