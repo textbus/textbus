f4decc8dd367da8e36e7b4329a8faf36fedcdc2b
tanbo <tanbo@cmcm.com>
2021-03-29 10:46:05 +0800

    feat: 导出默认配置项

--- src/create.ts
+++ src/create.ts
@@ -88,9 +88,64 @@ import {
   LinkJumpPlugin,
   TableEditPlugin,
   SourcecodeModePlugin,
-  OutlinesPlugin
+  OutlinesPlugin,
+  ToolFactory, DeviceOption, ComponentCreator
 } from './lib/ui/_api';
 import { Toolbar } from './lib/ui/toolbar/_api';
+
+export const defaultTools: Array<ToolFactory | ToolFactory[]> = [
+  [historyBackTool, historyForwardTool],
+  [insertObjectTool],
+  [headingTool],
+  [boldTool, italicTool, strikeThroughTool, underlineTool],
+  [olTool, ulTool],
+  [fontSizeTool, textIndentTool],
+  [colorTool, textBackgroundTool],
+  [insertParagraphBeforeTool, insertParagraphAfterTool],
+  [fontFamilyTool],
+  [linkTool, unlinkTool],
+  [imageTool],
+  [textAlignTool],
+  [tableTool],
+  [findTool],
+  [cleanTool]
+];
+
+export const defaultDeviceOptions: DeviceOption[] = [{
+  label: 'PC',
+  value: '100%',
+  default: true,
+}, {
+  label: 'iPhone5/SE',
+  value: '320px'
+}, {
+  label: 'iPhone6/7/8/X',
+  value: '375px'
+}, {
+  label: 'iPhone6/7/8 Plus',
+  value: '414px'
+}, {
+  label: 'iPad',
+  value: '768px'
+}, {
+  label: 'iPad Pro',
+  value: '1024px'
+}, {
+  label: 'A4',
+  value: '842px'
+}];
+
+export const defaultComponentLibrary: ComponentCreator[] = [
+  imageCardComponentExample,
+  todoListComponentExample,
+  baiduMapComponentExample,
+  jumbotronComponentExample,
+  wordExplainComponentExample,
+  timelineComponentExample,
+  progressComponentExample,
+  stepsComponentExample,
+  katexComponentExample,
+];
 
 export const defaultOptions: EditorOptions = {
   editingStyleSheets: [
@@ -139,61 +194,13 @@ export const defaultOptions: EditorOptions = {
   ],
   providers: [{
     provide: TOOLS,
-    useValue: [
-      [historyBackTool, historyForwardTool],
-      [insertObjectTool],
-      [headingTool],
-      [boldTool, italicTool, strikeThroughTool, underlineTool],
-      [olTool, ulTool],
-      [fontSizeTool, textIndentTool],
-      [colorTool, textBackgroundTool],
-      [insertParagraphBeforeTool, insertParagraphAfterTool],
-      [fontFamilyTool],
-      [linkTool, unlinkTool],
-      [imageTool],
-      [textAlignTool],
-      [tableTool],
-      [findTool],
-      [cleanTool]
-    ]
+    useValue: defaultTools
   }, {
     provide: DEVICE_OPTIONS,
-    useValue: [{
-      label: 'PC',
-      value: '100%',
-      default: true,
-    }, {
-      label: 'iPhone5/SE',
-      value: '320px'
-    }, {
-      label: 'iPhone6/7/8/X',
-      value: '375px'
-    }, {
-      label: 'iPhone6/7/8 Plus',
-      value: '414px'
-    }, {
-      label: 'iPad',
-      value: '768px'
-    }, {
-      label: 'iPad Pro',
-      value: '1024px'
-    }, {
-      label: 'A4',
-      value: '842px'
-    }]
+    useValue: defaultDeviceOptions
   }, {
     provide: COMPONENT_CREATORS,
-    useValue: [
-      imageCardComponentExample,
-      todoListComponentExample,
-      baiduMapComponentExample,
-      jumbotronComponentExample,
-      wordExplainComponentExample,
-      timelineComponentExample,
-      progressComponentExample,
-      stepsComponentExample,
-      katexComponentExample,
-    ]
+    useValue: defaultComponentLibrary
   }],
   plugins: [
     Toolbar,
