c0f7516551424ef08be887ba3844464f625f11b7
tanbo <tanbo@cmcm.com>
2021-05-11 10:07:13 +0800

    fix: 修复段落守护不及时的问题

--- src/extensions/plugins/guard-last-is-paragraph.plugin.ts
+++ src/extensions/plugins/guard-last-is-paragraph.plugin.ts
@@ -1,26 +1,24 @@
 import { Injectable } from '@tanbo/di';
 import { Subscription } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 
 import { TBPlugin } from '../../lib/plugin';
 import { RootComponent } from '../../lib/root-component';
-import { Fragment } from '../../lib/core/fragment';
 import { BlockComponent } from '../../lib/components/block.component';
-import { BrComponent } from '../../lib/core/component';
-import { TBSelection } from '../../lib/core/selection';
+import { BrComponent, TBSelection, Renderer, Fragment } from '../../lib/core/_api';
 
 @Injectable()
 export class GuardLastIsParagraphPlugin implements TBPlugin {
   private subs: Subscription[] = [];
 
   constructor(private rootComponent: RootComponent,
+              private renderer: Renderer,
               private selection: TBSelection) {
   }
 
   setup() {
     const rootComponent = this.rootComponent;
     const selection = this.selection;
-    this.subs.push(rootComponent.onChange.pipe(debounceTime(10)).subscribe(() => {
+    this.subs.push(this.renderer.onRendingBefore.subscribe(() => {
       const isEmpty = rootComponent.slot.length === 0;
       this.guardLastIsParagraph(rootComponent.slot);
       if (isEmpty && selection.firstRange) {
