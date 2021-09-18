4f390145e8f48c2f98b45b9b9d008242eff64499
tanbo <tanbo@cmcm.com>
2021-09-18 14:38:05 +0800

    chore: 添加 path，统一构建结果

--- packages/textbus/webpack.config.js
+++ packages/textbus/webpack.config.js
@@ -13,7 +13,26 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@textbus/textbus': path.resolve(__dirname, './src/public-api.ts'),
+      '@textbus/component-library-plugin': path.resolve(__dirname, '../component-library-plugin/src/public-api.ts'),
+      '@textbus/components': path.resolve(__dirname, '../components/src/public-api.ts'),
+      '@textbus/contextmenu-plugin': path.resolve(__dirname, '../contextmenu-plugin/src/public-api.ts'),
+      '@textbus/core': path.resolve(__dirname, '../core/src/public-api.ts'),
+      '@textbus/device-toggle-plugin': path.resolve(__dirname, '../device-toggle-plugin/src/public-api.ts'),
+      '@textbus/formatters': path.resolve(__dirname, '../formatters/src/public-api.ts'),
+      '@textbus/full-screen-plugin': path.resolve(__dirname, '../full-screen-plugin/src/public-api.ts'),
+      '@textbus/guard-end-block-plugin': path.resolve(__dirname, '../guard-end-block-plugin/src/public-api.ts'),
+      '@textbus/image-adn-video-drag-resize-plugin': path.resolve(__dirname, '../image-adn-video-drag-resize-plugin/src/public-api.ts'),
+      '@textbus/link-jump-tip-plugin': path.resolve(__dirname, '../link-jump-tip-plugin/src/public-api.ts'),
+      '@textbus/outlines-plugin': path.resolve(__dirname, '../outlines-plugin/src/public-api.ts'),
+      '@textbus/paste-upload-emitter-plugin': path.resolve(__dirname, '../paste-upload-emitter-plugin/src/public-api.ts'),
+      '@textbus/sourcecode-mode-plugin': path.resolve(__dirname, '../sourcecode-mode-plugin/src/public-api.ts'),
+      '@textbus/table-edit-enhance-plugin': path.resolve(__dirname, '../table-edit-enhance-plugin/src/public-api.ts'),
+      '@textbus/toolbar': path.resolve(__dirname, '../toolbar/src/public-api.ts'),
+      '@textbus/uikit': path.resolve(__dirname, '../uikit/src/public-api.ts')
+    }
   },
   devServer: {
     host: 'localhost',
