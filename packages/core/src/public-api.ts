fd5d559247812ea08d728d61bc0d0a879f01340a
tanbo <464328895@qq.com>
2022-03-16 22:53:05 +0800

    feat: 更新依赖

--- packages/core/src/public-api.ts
+++ packages/core/src/public-api.ts
@@ -1,4 +1,7 @@
 import 'reflect-metadata'
+export * from 'reflect-metadata'
+export * from '@tanbo/di'
+export * from '@tanbo/stream'
 
 export * from './_utils/make-error'
 export * from './foundation/_api'
