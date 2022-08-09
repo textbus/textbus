db66e77c400e6bac74249afc704ecd0f56b8ae96
cricetus <tinycricetus@qq.com>
2022-08-09 20:24:03 +0800

    feat: 应用 outer 类型至 font-size 与 color 格式器

---
+++ packages/editor/src/formatters/outer-style.formatter.ts
@@ -0,0 +1,46 @@
+import { OuterFormatter, FormatType, VElement } from '@textbus/core'
+import { Matcher, MatchRule } from './matcher'
+
+export class OuterStyleFormatLoader extends Matcher {
+  constructor(public styleName: string, formatter: OuterFormatter, rule: MatchRule) {
+    super(formatter, rule)
+  }
+
+  read(node: HTMLElement) {
+    return this.extractFormatData(node, {
+      styleName: this.styleName
+    }).styles[this.styleName]
+  }
+}
+
+export class OuterStyleFormatter implements OuterFormatter {
+  type: FormatType.Outer = FormatType.Outer
+
+  constructor(public name: string,
+              public styleName: string) {
+  }
+
+  render(node: VElement | null, formatValue: string): VElement | void {
+    if (node) {
+      node.styles.set(this.styleName, formatValue)
+    } else {
+      const el = new VElement('span')
+      el.styles.set(this.styleName, formatValue)
+      return el
+    }
+  }
+}
+
+export const colorFormatter = new OuterStyleFormatter('color', 'color')
+export const colorFormatLoader = new OuterStyleFormatLoader('color', colorFormatter, {
+  styles: {
+    color: /.+/
+  }
+})
+
+export const fontSizeFormatter = new OuterStyleFormatter('fontSize', 'fontSize')
+export const fontSizeFormatLoader = new OuterStyleFormatLoader('fontSize', fontSizeFormatter, {
+  styles: {
+    fontSize: /.+/
+  }
+})
