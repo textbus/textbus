62cdb851ee2d539867619810262d586837871c48
tanbo <464328895@qq.com>
2022-11-08 22:45:37 +0800

    chore: 使用 rollup + pnpm 管理多包

---
+++ packages/collaborate/rollup.config.ts
@@ -0,0 +1,20 @@
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+
+export default {
+  input: 'src/public-api.ts',
+  output: [
+    {
+      file: './bundles/index.js',
+      format: 'cjs'
+    },
+    {
+      file: './bundles/index.esm.js',
+      format: 'esm'
+    }
+  ],
+  plugins: [
+    commonjs(),
+    typescript()
+  ]
+}
