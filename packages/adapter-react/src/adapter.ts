a1c5091f485187347e3149f18d64f21d587d703d
tbhuabi <464328895@qq.com>
2023-08-16 21:22:30 +0800

    feat: 添加 react 适配器

---
+++ packages/adapter-react/src/adapter.ts
@@ -0,0 +1,151 @@
+import { createElement, JSX, ReactNode, useEffect, useState } from 'react'
+import { Subject } from '@tanbo/stream'
+import {
+  Component,
+  ComponentInstance,
+  ExtractComponentInstanceType,
+  makeError,
+  replaceEmpty,
+  Slot,
+  VElement,
+  VTextNode
+} from '@textbus/core'
+import { DomAdapter } from '@textbus/platform-browser'
+
+// hack start
+// 在 composition 输入时，浏览器会默认删除占位节点（<br>），这会导致 react 在 diff 时报错
+const oldRemoveChild = Node.prototype.removeChild
+Node.prototype.removeChild = function (this: any, child: any) {
+  if (child) {
+    if (!child.parentNode) {
+      return child
+    }
+  }
+  return oldRemoveChild.call(this, child)
+}
+// hack end
+
+const adapterError = makeError('ReactAdapter')
+
+export interface ViewComponentProps<T extends Component = Component> {
+  component: ExtractComponentInstanceType<T>
+  rootRef: ((rootNode: HTMLElement) => void)
+}
+
+export interface ReactAdapterComponents {
+  [key: string]: (props: ViewComponentProps) => JSX.Element
+}
+
+export class Adapter extends DomAdapter<JSX.Element, JSX.Element> {
+  onViewUpdated = new Subject<void>()
+
+  private components: Record<string, (props: {component: ComponentInstance}) => JSX.Element> = {}
+
+  constructor(components: ReactAdapterComponents,
+              mount: (host: HTMLElement, root: JSX.Element) => (void | (() => void))) {
+    super(mount)
+    Object.keys(components).forEach(key => {
+      this.components[key] = (props: {component: ComponentInstance}) => {
+        const component = props.component
+        const [updateKey, refreshUpdateKey] = useState(Math.random())
+
+        useEffect(() => {
+          const sub = component.changeMarker.onChange.subscribe(() => {
+            if (component.changeMarker.dirty) {
+              refreshUpdateKey(Math.random())
+            }
+          })
+          return () => {
+            sub.unsubscribe()
+          }
+        }, [])
+        useEffect(() => {
+          this.onViewUpdated.next()
+        }, [updateKey])
+        return components[key]({
+          component,
+          rootRef: (rootNode: HTMLElement) => {
+            if (rootNode) {
+              this.componentRootElementCaches.set(component, rootNode)
+            } else {
+              this.componentRootElementCaches.delete(component)
+            }
+          }
+        })
+      }
+    })
+  }
+
+  componentRender(component: ComponentInstance): JSX.Element {
+    const comp = this.components[component.name] || this.components['*']
+    if (comp) {
+      component.changeMarker.rendered()
+      return createElement(comp, {
+        key: component.id,
+        component
+      })
+    }
+    throw adapterError(`cannot found view component \`${component.name}\`!`)
+  }
+
+  slotRender(slot: Slot, slotHostRender: (children: Array<VElement | VTextNode | ComponentInstance>) => VElement): JSX.Element {
+    const vElement = slot.toTree(slotHostRender)
+    this.slotRootVElementCaches.set(slot, vElement)
+
+    const vNodeToJSX = (vNode: VElement) => {
+      const children: ReactNode[] = []
+
+      for (let i = 0; i < vNode.children.length; i++) {
+        const child = vNode.children[i]
+        if (child instanceof VElement) {
+          children.push(vNodeToJSX(child))
+        } else if (child instanceof VTextNode) {
+          children.push(replaceEmpty(child.textContent))
+        } else {
+          children.push(this.componentRender(child))
+          child.changeMarker.rendered()
+        }
+      }
+      const props: any = {
+        ...(Array.from(vNode.attrs).reduce((a, b) => {
+          a[b[0]] = b[1]
+          return a
+        }, {}))
+      }
+      if (vNode.classes.size) {
+        props.className = Array.from(vNode.classes).join(' ')
+      }
+      if (vNode.styles) {
+        props.style = Array.from(vNode.styles).reduce((a, b) => {
+          a[b[0]] = b[1]
+          return a
+        }, {})
+      }
+      return createElement(vNode.tagName, props, ...children)
+    }
+
+    const refFn = (nativeNode: HTMLElement) => {
+      if (!nativeNode) {
+        this.slotRootNativeElementCaches.remove(nativeNode)
+      } else {
+        this.slotRootNativeElementCaches.set(slot, nativeNode)
+      }
+    }
+    const currentRef = vElement.attrs.get('ref')
+    if (currentRef) {
+      vElement.attrs.set('ref', (v: HTMLElement) => {
+        refFn(v)
+        if (typeof currentRef === 'function') {
+          currentRef(v)
+        } else if (!currentRef.current) {
+          currentRef.current = v
+        }
+      })
+    } else {
+      vElement.attrs.set('ref', refFn)
+    }
+
+    slot.changeMarker.rendered()
+    return vNodeToJSX(vElement)
+  }
+}
