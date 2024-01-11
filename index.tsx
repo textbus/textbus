9bae3eafe7fd965755e580422d8c121adc883aa4
tbhuabi <464328895@qq.com>
2024-01-11 21:38:12 +0800

    feat: 删除 Formatter 和 Attribute 的 setup、destroy 勾子

--- index.tsx
+++ index.tsx
@@ -20,127 +20,139 @@ import {
 import { createRoot } from 'react-dom/client'
 import { Adapter, ViewComponentProps } from '@textbus/adapter-react'
 
-const adapter = new Adapter({
-  RootComponent: App,
-  ParagraphComponent: Paragraph
-}, (host, root) => {
-  const app = createRoot(host)
-  app.render(root)
-  return () => {
-    app.unmount()
+async function createEditor() {
+  if (!Intl.Segmenter) {
+    const polyfill = await import('intl-segmenter-polyfill/dist/bundled');
+    (Intl as any).Segmenter = await polyfill.createIntlSegmenterPolyfill()
   }
-})
-const browserModule = new BrowserModule({
-  adapter,
-  renderTo() {
-    return document.getElementById('editor')!
-  },
-  // useContentEditable: true
-})
+  const adapter = new Adapter({
+    RootComponent: App,
+    ParagraphComponent: Paragraph
+  }, (host, root) => {
+    const app = createRoot(host)
+    app.render(root)
+    return () => {
+      app.unmount()
+    }
+  })
+  const browserModule = new BrowserModule({
+    adapter,
+    renderTo() {
+      return document.getElementById('editor')!
+    },
+    // useContentEditable: true
+  })
 
-const textbus = new Textbus({
-  imports: [
-    browserModule
-  ]
-})
+  const textbus = new Textbus({
+    imports: [
+      browserModule
+    ]
+  })
 
-const fontSizeFormatter = new Formatter<string>('fontSize', {
-  columned: false,
-  inheritable: true,
-  priority: 0,
-  render(children: Array<VElement | VTextNode | ComponentInstance>, formatValue: string): VElement | FormatHostBindingRender {
-    return createVNode('span', {
-      style: {
-        fontSize: formatValue
+  const fontSizeFormatter = new Formatter<string>('fontSize', {
+    columned: false,
+    inheritable: true,
+    priority: 0,
+    render(children: Array<VElement | VTextNode | ComponentInstance>, formatValue: string): VElement | FormatHostBindingRender {
+      return createVNode('span', {
+        style: {
+          fontSize: formatValue
+        }
+      }, children)
+    }
+  })
+
+  const rootComponent = defineComponent({
+    name: 'RootComponent',
+    type: ContentType.BlockComponent,
+    validate() {
+      return {
+        slots: [
+          new Slot([
+            ContentType.Text,
+            ContentType.BlockComponent
+          ])
+        ]
       }
-    }, children)
-  }
-})
-
-const rootComponent = defineComponent({
-  name: 'RootComponent',
-  type: ContentType.BlockComponent,
-  validate() {
-    return {
-      slots: [
-        new Slot([
-          ContentType.Text,
-          ContentType.BlockComponent
-        ])
-      ]
-    }
-  },
-  setup() {
-    const slot = useSelf().slots.get(0)!
-    for (let i = 0; i < 50; i++) {
-      const p = paragraphComponent.createInstance(useContext())
-      p.slots.first.insert(Math.random().toString(16), fontSizeFormatter, '23px')
-      slot.insert(p)
-    }
-  }
-})
-
-const paragraphComponent = defineComponent({
-  name: 'ParagraphComponent',
-  type: ContentType.BlockComponent,
-  validate(_, initData) {
-    return {
-      slots: initData?.slots || [new Slot([
-        ContentType.Text
-      ])]
-    }
-  },
-  setup() {
-    const context = useContext()
-    const commander = useContext(Commander)
-    const selection = useContext(Selection)
-    const self = useSelf()
-
-    onBreak(ev => {
-      ev.preventDefault()
-      const nextContent = ev.target.cut(ev.data.index)
-      const p = paragraphComponent.createInstance(context, {
-        slots: [nextContent]
-      })
-      commander.insertAfter(p, self)
-      selection.selectFirstPosition(p)
-    })
-
-    return {
-      show() {
-        return 'dfsafdas'
+    },
+    setup() {
+      const slot = useSelf().slots.get(0)!
+      for (let i = 0; i < 50; i++) {
+        const p = paragraphComponent.createInstance(useContext())
+        p.slots.first.insert(Math.random().toString(16), fontSizeFormatter, '23px')
+        slot.insert(p)
       }
     }
-  }
-})
+  })
 
-const rootModel = rootComponent.createInstance(textbus)
+  const paragraphComponent = defineComponent({
+    name: 'ParagraphComponent',
+    type: ContentType.BlockComponent,
+    validate(_, initData) {
+      return {
+        slots: initData?.slots || [new Slot([
+          ContentType.Text
+        ])]
+      }
+    },
+    setup() {
+      const context = useContext()
+      const commander = useContext(Commander)
+      const selection = useContext(Selection)
+      const self = useSelf()
 
-textbus.render(rootModel)
-
-function App(props: ViewComponentProps<typeof rootComponent>) {
-  const slot = props.component.slots.first
-
-  return (
-    <div className="xxxx">
-      {
-        adapter.slotRender(slot, children => {
-          return createVNode('div', {
-            'textbus-document': 'true',
-            ref: props.rootRef
-          }, children)
+      onBreak(ev => {
+        ev.preventDefault()
+        const nextContent = ev.target.cut(ev.data.index)
+        const p = paragraphComponent.createInstance(context, {
+          slots: [nextContent]
         })
+        commander.insertAfter(p, self)
+        selection.selectFirstPosition(p)
+      })
+
+      return {
+        show() {
+          return 'dfsafdas'
+        }
       }
-    </div>
-  )
+    }
+  })
+
+  const rootModel = rootComponent.createInstance(textbus)
+
+
+  textbus.render(rootModel)
+  // 从这里开始创建编辑器
+
+// textbus.render(rootModel)
+
+  function App(props: ViewComponentProps<typeof rootComponent>) {
+    const slot = props.component.slots.first
+
+    return (
+      <div className="xxxx">
+        {
+          adapter.slotRender(slot, children => {
+            return createVNode('div', {
+              'textbus-document': 'true',
+              ref: props.rootRef
+            }, children)
+          })
+        }
+      </div>
+    )
+  }
+
+  function Paragraph(props: ViewComponentProps<typeof paragraphComponent>) {
+    const slot = props.component.slots.first
+    return (
+      adapter.slotRender(slot, children => {
+        return createVNode('p', { ref: props.rootRef }, children)
+      })
+    )
+  }
 }
 
-function Paragraph(props: ViewComponentProps<typeof paragraphComponent>) {
-  const slot = props.component.slots.first
-  return (
-    adapter.slotRender(slot, children => {
-      return createVNode('p', { ref: props.rootRef }, children)
-    })
-  )
-}
+createEditor()
 
