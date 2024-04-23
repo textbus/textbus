c52c27003782ccc48bc11095c3f20d25aebe2cea
tbhuabi <464328895@qq.com>
2024-04-24 00:03:27 +0800

    feat: 优化协作初始化流程

--- packages/collaborate/src/collaborate-module.ts
+++ packages/collaborate/src/collaborate-module.ts
@@ -1,14 +1,48 @@
-import { History, Module } from '@textbus/core'
+import { History, Module, Textbus } from '@textbus/core'
 import { Provider } from '@viewfly/core'
+import { WebsocketProvider } from 'y-websocket'
 
 import { Collaborate } from './collaborate'
+import { UserActivity, UserInfo } from './user-activity'
+
+export interface CollaborateConfig {
+  url: string
+  roomName: string
+  userinfo: UserInfo
+}
 
 export class CollaborateModule implements Module {
   providers: Provider[] = [
     Collaborate,
+    UserActivity,
     {
       provide: History,
       useExisting: Collaborate
+    }, {
+      provide: WebsocketProvider,
+      useFactory: (collab: Collaborate) => {
+        return new WebsocketProvider(this.config.url, this.config.roomName, collab.yDoc)
+      },
+      deps: [Collaborate]
     }
   ]
+
+  constructor(public config: CollaborateConfig) {
+  }
+
+  setup(textbus: Textbus): Promise<(() => void) | void> | (() => void) | void {
+    const provide = textbus.get(WebsocketProvider)
+    const userActivity = textbus.get(UserActivity)
+    userActivity.init(this.config.userinfo)
+    return new Promise<() => void>((resolve) => {
+      provide.on('sync', (is: boolean) => {
+        if (is) {
+          resolve(() => {
+            provide.disconnect()
+            userActivity.destroy()
+          })
+        }
+      })
+    })
+  }
 }
